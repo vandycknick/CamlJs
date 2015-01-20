3da6208f3fb2761d4a65381688d7e66b70863e41
vdyckn <vandyck.nick@outlook.com>
2015-01-20 18:15:35 +0100

    removed copy.cmd batc -> replaced by gulpfile

--- copy.cmd
+++
@@ -1,6 +0,0 @@
-"C:\Program Files (x86)\Microsoft SDKs\TypeScript\1.0\tsc" --declaration CamlJs\camljs.ts --out camljs.d.ts
-copy CamlJs\camljs.js Nuget\content\Scripts
-copy CamlJs\camljs.js CamlJs.TestApp\Scripts
-copy camljs.d.ts ..\DefinitelyTyped\camljs
-copy CamlJs\camljs.js ..\camljs-console\Scripts\
-copy camljs.d.ts ..\camljs-console\Scripts\typings\camljs
